2361d52f194446d52b46bb754e6dbedf50f21ff2
Vitchapo <118248210+Vitchapo@users.noreply.github.com>
2026-03-22 14:26:31 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+const SHEET_ID           = "1oQ4GC7sTXCikDYdQLY96CwPSUaeIoOJ3x6mYUjlZuYI";
+const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1482907096134455348/w5K5LenWpbxAQ3-WfIHO6UumW4-KdkQqoEg1MeuYSwy22D3aPkM_cxts0C8ngngfUfFp";
